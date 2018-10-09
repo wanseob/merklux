bea3429a277b463bb745c62b986a44d392360e43
James Lim <email@wanseob.com>
2018-10-09 17:59:01 +0900

    Fix npm run coverage error

--- test/utils.js
+++ test/utils.js
@@ -1,12 +1,4 @@
-const hexToString = hex => {
-  const hexCodes = hex.startsWith('0x') ? hex.substr(2) : hex
-  let str = ''
-  let i
-  for (i = 0; (i < hexCodes.length && hexCodes.substr(i, 2) !== '00'); i += 2) {
-    str += String.fromCharCode(parseInt(hexCodes.substr(i, 2), 16))
-  }
-  return str
-}
+const readline = require('readline')
 
 const toNodeObject = (depth, label, node) => {
   return {
@@ -20,8 +12,7 @@ const toNodeObject = (depth, label, node) => {
 
 const progress = {
   log: async (output, ms) => {
-    process.stdout.clearLine()
-    process.stdout.cursorTo(0)
+    readline.cursorTo(process.stdout, 0)
     process.stdout.write(`Progress >>\t${output}`)
     if (ms) {
       let sleep = () => new Promise(resolve => setTimeout(resolve, ms))
@@ -29,14 +20,12 @@ const progress = {
     }
   },
   close: () => {
-    process.stdout.clearLine()
-    process.stdout.cursorTo(0)
+    readline.cursorTo(process.stdout, 0)
     process.stdout.write('')
   }
 }
 
 module.exports = {
-  hexToString,
   toNodeObject,
   progress
 }
