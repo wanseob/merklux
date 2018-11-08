91675ace66fea4ff511faf994eaf2c860d782fd9
James Lim <email@wanseob.com>
2018-11-09 04:24:01 +0900

    Fix test script

---
+++ .solcover.js
@@ -0,0 +1,7 @@
+module.exports = {
+  compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network development',
+  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network development',
+  skipFiles: [
+    'contracts/Migrations.sol'
+  ]
+}
